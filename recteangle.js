ca2ebbbda19f3dee56c9b98ac279c9e6491cd1c7
Md Sayed Hossain <freelancersayed45@gmail.com>
2024-02-12 23:57:30 +0600

    modifi the seccond cardt

---
+++ recteangle.js
@@ -0,0 +1,6 @@
+function calculateRectangleArea(){
+    const lengthInput = document.getElementById('rectangle_lenght');
+    const lengthText = lengthInput.value
+    const length = parseFloat(lengthText)
+    console.log(length)
+}
